9376b81f1a375ffd94f6a61d0031a5f39160829f
Owen Young <theowenyoung@gmail.com>
2020-08-10 06:48:22 +0800

    feat:

---
+++ src/triggers/webhook.js
@@ -0,0 +1,10 @@
+const Parser = require("rss-parser");
+const log = require("../log");
+module.exports = class {
+  async run({ helpers, options }) {
+    // if need
+    return {
+      results: [],
+    };
+  }
+};
