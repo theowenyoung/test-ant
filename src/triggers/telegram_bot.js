abcf8bd481e56bfb371cdd8369ef7486c7b075d1
Owen Young <theowenyoung@gmail.com>
2020-08-10 13:25:23 +0800

    chore: manual update

--- src/triggers/telegram_bot.js
+++ src/triggers/telegram_bot.js
@@ -81,6 +81,7 @@ module.exports = class {
       resultsArray.forEach((item) => {
         // @ts-ignore
         const message = item.message;
+        message.update_id = item.update_id;
         const messageType = _messageTypes.find((messageType) => {
           return message[messageType];
         });
